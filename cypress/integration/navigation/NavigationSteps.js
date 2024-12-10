4ad16046d22a9d96cf32d53f58146d751ea38036
andrea.buenaventura <andrea.buenaventura@globant.com>
2024-12-10 08:26:47 -0500

    #56

--- cypress/integration/navigation/NavigationSteps.js
+++ cypress/integration/navigation/NavigationSteps.js
@@ -43,6 +43,7 @@ When('I click the close sign up button',()=>{
 })
 
 When('I click on the Cart button', ()=>{
+  cy.intercept('GET', '/cart.html').as('getCart');
   homepage.clickOnCarButton();
 })
 When('I click on the Home button',()=>{
