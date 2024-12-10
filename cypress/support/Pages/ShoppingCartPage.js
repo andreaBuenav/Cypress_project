1c620fe3a8246edaf966bed14c2e5593e4d74020
andrea.buenaventura <andrea.buenaventura@globant.com>
2024-12-10 08:46:07 -0500

    Update #65

--- cypress/support/Pages/ShoppingCartPage.js
+++ cypress/support/Pages/ShoppingCartPage.js
@@ -1,20 +1,19 @@
 //faker
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
 //Elements of the page
-const Title = '#page-wrapper > div > div.col-lg-8 > h2';
-const NameInput = '#name'
-const CountryInput = '#country'
-const CityInput = '#city'
-const CreditCardInput = '#card'
-const MonthInput = '#month'
-const YearInput = '#year'
-const OrderModal = '#orderModal'
-const CloseOrder = '#orderModal button.btn.btn-secondary'
-const PurchaseOrder = '#orderModal btn btn-primary'
-const PlaceOrder ='.btn.btn-success'
-const Ok ='button.confirm.btn.btn-lg.btn-primary'
-
+const Title = "#page-wrapper > div > div.col-lg-8 > h2";
+const NameInput = "#name";
+const CountryInput = "#country";
+const CityInput = "#city";
+const CreditCardInput = "#card";
+const MonthInput = "#month";
+const YearInput = "#year";
+const OrderModal = "#orderModal";
+const CloseOrder = "#orderModal button.btn.btn-secondary";
+const PurchaseOrder = "#orderModal btn btn-primary";
+const PlaceOrder = ".btn.btn-success";
+const Ok = "button.confirm.btn.btn-lg.btn-primary";
 
 //variables
 const name = faker.person.fullName();
@@ -24,36 +23,31 @@ const credit = faker.number.bigInt({ min: 1000000000, max: 9999999999 });
 const month = faker.date.month();
 const year = 2024;
 
-class ShoppingCartPage{
-    navigateToCartPage(){
-      cy.visit(`${Cypress.config().baseUrl}/cart.html`);
-    }
+class ShoppingCartPage {
+  navigateToCartPage() {
+    cy.visit(`${Cypress.config().baseUrl}/cart.html`);
+  }
 
-    response(expectedStatusCode, url) {
-        cy.intercept("GET", url).as("responseCheck");
-        cy.wait("@responseCheck")
-          .its("response.statusCode")
-          .should("eq", expectedStatusCode);
-      }
+  response(expectedStatusCode, url) {
+    cy.intercept("GET", `${Cypress.config().baseUrl}/cart.html`).as("getCart");
+    cy.wait("@getCart")
+      .its("response.statusCode")
+      .should("eq", expectedStatusCode);
+  }
 
-      clickOnDelete(){
+  clickOnDelete() {}
 
-      }
-
-      PurchaseOrder(){
-        cy.get(PlaceOrder).click();
-        cy.get(OrderModal).should('be.visible');
-        cy.get(NameInput).type(name);
-        cy.get(CountryInput).type(country);
-        cy.get(CityInput).type(city);
-        cy.get(CreditCardInput).type(credit);
-        cy.get(MonthInput).type(month);
-        cy.get(YearInput).type(year);
-        cy.get(PurchaseOrder).click();
-        cy.get(Ok).should('be.visible').wait(2000).click();
-
-      }
-
-    
+  PurchaseOrder() {
+    cy.get(PlaceOrder).click();
+    cy.get(OrderModal).should("be.visible");
+    cy.get(NameInput).type(name);
+    cy.get(CountryInput).type(country);
+    cy.get(CityInput).type(city);
+    cy.get(CreditCardInput).type(credit);
+    cy.get(MonthInput).type(month);
+    cy.get(YearInput).type(year);
+    cy.get(PurchaseOrder).click();
+    cy.get(Ok).should("be.visible").wait(2000).click();
+  }
 }
 export default ShoppingCartPage;
