c2a17398f49ab363369526196c00485203ba54b6
andrea.buenaventura <andrea.buenaventura@globant.com>
2024-12-10 12:23:43 -0500

    Updaate #119

--- cypress/support/Pages/HomePage.js
+++ cypress/support/Pages/HomePage.js
@@ -168,14 +168,13 @@ verifyResponsePhones(){
 verifyResponseLaptops(){
   cy.wait(1000);
   cy.get('a.hrefch').then(($links) => {
-    expect($links).to.have.lengthOf(15);
+    expect($links).to.have.lengthOf(8);
     const productIds = [];
     $links.each((_, link) => {
-      const productId = link.href.split('=')[8];
+      const productId = link.href.split('=')[1];
       productIds.push(productId);
     });
     expect(productIds.sort()).to.deep.equal(['8', '9', '10', '11', '12', '13', '14', '15']);
-    cy.get('a.hrefch').should('have.length', 8);
   });
 }
 
